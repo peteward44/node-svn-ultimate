4d5cd22a586ae6f6325d0328158ad89a85d8f51f
pineoc <kingsuk92@gmail.com>
2019-02-21 20:04:10 +0900

    Changed double qoute to single qoute, commit message on code

--- test/commands.js
+++ test/commands.js
@@ -10,10 +10,10 @@ var fs = require('fs-extra');
 var createRepo = require('./createLocalSvnRepo.js' );
 
 var testServer = '';
-var nonExistantUrl = "https://fud.com/svn/work";
-var tempTestDir = "temp";
+var nonExistantUrl = 'https://fud.com/svn/work';
+var tempTestDir = 'temp';
 var repoTestDir = path.join( tempTestDir, 'repo' );
-var checkoutTestDir = path.join( tempTestDir, "out_works" );
+var checkoutTestDir = path.join( tempTestDir, 'out_works' );
 
 
 describe('node-svn-ultimate', function() {
@@ -40,7 +40,7 @@ describe('node-svn-ultimate', function() {
 			} );
 		});
 		it('checkout invalid url', function(done) {
-			var p = path.join( tempTestDir, "out_invalid" );
+			var p = path.join( tempTestDir, 'out_invalid' );
 			svn.commands.checkout( nonExistantUrl, p, { quiet: true }, function( err ) {
 				should.exist(err);
 				done();
@@ -50,8 +50,8 @@ describe('node-svn-ultimate', function() {
 		
 
 		it('add', function(done) {
-			var fp = path.join( checkoutTestDir, "test2.txt" );
-			fs.writeFileSync( fp, "aa" );
+			var fp = path.join( checkoutTestDir, 'test2.txt' );
+			fs.writeFileSync( fp, 'aa' );
 			svn.commands.add( fp, function( err ) {
 				should.not.exist(err);
 				done();
@@ -59,7 +59,7 @@ describe('node-svn-ultimate', function() {
 		});
 
 		it('commit', function(done) {
-			svn.commands.commit( checkoutTestDir, { msg: 'test commit' }, function( err ) {
+			svn.commands.commit( checkoutTestDir, { msg: 'Added test2.txt' }, function( err ) {
 				should.not.exist(err);
 				done();
 			} );
@@ -74,7 +74,7 @@ describe('node-svn-ultimate', function() {
 		});
 		
 		// it('patch', function(done) {
-			// var p = path.join( tempTestDir, "test_patch.patch" );
+			// var p = path.join( tempTestDir, 'test_patch.patch' );
 			// svn.commands.patch( p,  function( err ) {
 				// should.not.exist(err);
 				// should.ok( fs.readFileSync( p ).toString() === 'aa' );
@@ -83,7 +83,7 @@ describe('node-svn-ultimate', function() {
 		// });
 		
 		it('revert', function(done) {
-			var p = path.join( checkoutTestDir, "test2.txt" );
+			var p = path.join( checkoutTestDir, 'test2.txt' );
 			fs.writeFileSync( p, 'bbbbbb' );
 			svn.commands.revert( p, function( err ) {
 				should.not.exist(err);
@@ -93,29 +93,29 @@ describe('node-svn-ultimate', function() {
 		});
 		
 		it('cat', function(done) {
-			svn.commands.cat( testServer + "/test2.txt", function( err, data ) {
+			svn.commands.cat( testServer + '/test2.txt', function( err, data ) {
 				should.not.exist(err);
 				should.ok( data.length > 0 );
-				should.ok( data === "aa" );
+				should.ok( data === 'aa' );
 				done();
 			} );
 		});
 		
 		it('cat fails on non-existant file', function(done) {
-			svn.commands.cat( testServer + "/bower2.json", { quiet: true }, function( err, data ) {
+			svn.commands.cat( testServer + '/bower2.json', { quiet: true }, function( err, data ) {
 				should.exist(err);
 				done();
 			} );
 		});
 		
 		it('copy', function(done) {
-			svn.commands.copy( testServer + "/test2.txt", testServer + "/test3.txt", "copy commit", function( err ) {
+			svn.commands.copy( testServer + '/test2.txt', testServer + '/test3.txt', 'Copy test2 to test3', function( err ) {
 				should.not.exist(err);
 				// make sure copied file exists
-				svn.commands.cat( testServer + "/test3.txt", function( err2, data ) {
+				svn.commands.cat( testServer + '/test3.txt', function( err2, data ) {
 					should.not.exist(err2);
 					should.ok( data.length > 0 );
-					should.ok( data === "aa" );
+					should.ok( data === 'aa' );
 					done();
 				} );
 			} );
@@ -147,16 +147,16 @@ describe('node-svn-ultimate', function() {
     it('merge', function(done) {
       svn.commands.info( testServer, function( err, data ) {
         var oldRev = data.entry.$.revision;
-        var fp = path.join( checkoutTestDir, "test2.txt" );
+        var fp = path.join( checkoutTestDir, 'test2.txt' );
         var oldContent = fs.readFileSync(fp);
-        fs.writeFileSync( fp, "aabb" );
-				svn.commands.commit( checkoutTestDir, { msg: 'test commit (merge)' }, function( err ) {
+        fs.writeFileSync( fp, 'aabb' );
+				svn.commands.commit( checkoutTestDir, { msg: 'Update test2.txt (aa -> aabb)' }, function( err ) {
 					svn.commands.info( testServer, function( err, data ) {
 						var newRev = data.entry.$.revision;
 						var mergeOption = newRev + ':' + oldRev;
 						svn.commands.merge( fp, { revision: mergeOption }, function( err ) {
 							should.not.exist(err);
-							svn.commands.commit( checkoutTestDir, { msg: 'test commit 2 (merge)' }, function( err ) {
+							svn.commands.commit( checkoutTestDir, { msg: 'Merged test2.txt ' }, function( err ) {
                 var newContent = fs.readFileSync(fp);
                 should.ok( newContent.toString('utf8') === oldContent.toString('utf8') );
                 done();
@@ -183,7 +183,8 @@ describe('node-svn-ultimate', function() {
 			var opt = {showRevs: 'eligible'};
 			svn.commands.mergeinfo (src, target, opt, function( err, data ) {
 				should.not.exist(err);
-				should.exist(data);
+				should.ok( data.length > 0 );
+				should.equal( data, 'r3\nr4\n' );
 				done();
 			} );
 		});
@@ -197,15 +198,15 @@ describe('node-svn-ultimate', function() {
 		});
 				
 		it('move', function(done) {
-			svn.commands.move( testServer + "/test3.txt", testServer + "/test4.txt", "move commit", function( err ) {
+			svn.commands.move( testServer + '/test3.txt', testServer + '/test4.txt', 'Move test3.txt to test4.txt', function( err ) {
 				should.not.exist(err);
 				// make sure moved file exists
-				svn.commands.cat( testServer + "/test4.txt", function( err2, data ) {
+				svn.commands.cat( testServer + '/test4.txt', function( err2, data ) {
 					should.not.exist(err2);
 					should.ok( data.length > 0 );
-					should.ok( data === "aa" );
+					should.ok( data === 'aa' );
 					// make sure old file no longer exists
-					svn.commands.cat( testServer + "/test3.txt", function( err3, data ) {
+					svn.commands.cat( testServer + '/test3.txt', function( err3, data ) {
 						should.exist(err3);
 						done();
 					} );
@@ -214,10 +215,10 @@ describe('node-svn-ultimate', function() {
 		});
 		
 		it('del', function(done) {
-			svn.commands.del( testServer + "/test4.txt", "del commit", function( err ) {
+			svn.commands.del( testServer + '/test4.txt', 'Del text4.txt', function( err ) {
 				should.not.exist(err);
 				// make sure file deleted
-				svn.commands.cat( testServer + "/test4.txt", function( err2, data ) {
+				svn.commands.cat( testServer + '/test4.txt', function( err2, data ) {
 					should.exist(err2);
 					done();
 				} );
@@ -226,31 +227,30 @@ describe('node-svn-ultimate', function() {
 
 		it('export - single file', function(done) {
 			var exportPath = path.join( tempTestDir, 'test2.txt' );
-			svn.commands.export( testServer + "/test2.txt", exportPath, function( err ) {
+			svn.commands.export( testServer + '/test2.txt', exportPath, function( err ) {
 				should.not.exist(err);
 				should.ok( fs.existsSync( exportPath ) );
-				should.ok( fs.readFileSync( exportPath ).toString() === "aa" );
+				should.ok( fs.readFileSync( exportPath ).toString() === 'aa' );
 				done();
 			} );
 		});
 		
 		it('import - single file', function(done) {
 			var importPath = path.join( tempTestDir, 'test2.txt' );
-			svn.commands.import( importPath, testServer + "/test20.txt", 'import msg', function( err ) {
+			svn.commands.import( importPath, testServer + '/test20.txt', 'Import test2.txt to test20.txt', function( err ) {
 				should.not.exist(err);
-				svn.commands.cat( testServer + "/test20.txt", function( err2, data ) {
+				svn.commands.cat( testServer + '/test20.txt', function( err2, data ) {
 					should.not.exist(err2);
 					should.ok( data.length > 0 );
-					should.ok( data === "aa" );
+					should.ok( data === 'aa' );
 					done();
 				} );
 			} );
 		});
 		
 		it('mucc - single mkdir command', function(done) {
-			svn.commands.mucc( [ "mkdir " + testServer + "/testdir" ], "mucc commit", function( err, data ) {
+			svn.commands.mucc( [ 'mkdir ' + testServer + '/testdir' ], 'mucc, Added testdir', function( err, data ) {
 				should.not.exist(err);
-				//console.log( data );
 				done();
 			} );
 		});
@@ -258,18 +258,18 @@ describe('node-svn-ultimate', function() {
 		it('update working copy', function(done) {
 			svn.commands.update( checkoutTestDir, function( err ) {
 				should.not.exist(err);
-				should.ok( fs.statSync( path.join( checkoutTestDir, "test20.txt" ) ).isFile() );
-				should.ok( fs.statSync( path.join( checkoutTestDir, "testdir" ) ).isDirectory() );
+				should.ok( fs.statSync( path.join( checkoutTestDir, 'test20.txt' ) ).isFile() );
+				should.ok( fs.statSync( path.join( checkoutTestDir, 'testdir' ) ).isDirectory() );
 				done();
 			} );
 		});
 
 		it('lock: local file in working copy', function(done) {
-			var p = path.join( checkoutTestDir, "test20.txt" );
+			var p = path.join( checkoutTestDir, 'test20.txt' );
 			svn.commands.lock( p, function( err, data ) {
 				should.not.exist(err);
 				// trying to delete the file should fail
-				svn.commands.del( testServer + "/test20.txt", "del commit", function( err2 ) {
+				svn.commands.del( testServer + '/test20.txt', 'Del test20.txt (locked, fail)', function( err2 ) {
 					should.exist(err2);
 					done();
 				} );
@@ -277,11 +277,11 @@ describe('node-svn-ultimate', function() {
 		});
 
 		it('unlock: local file in working copy', function(done) {
-			var p = path.join( checkoutTestDir, "test20.txt" );
+			var p = path.join( checkoutTestDir, 'test20.txt' );
 			svn.commands.unlock( p, function( err, data ) {
 				should.not.exist(err);
 				// trying to delete the file should succeed
-				svn.commands.del( testServer + "/test20.txt", "del commit", function( err2 ) {
+				svn.commands.del( testServer + '/test20.txt', 'Del test20.txt (unlocked, success)', function( err2 ) {
 					should.not.exist(err2);
 					done();
 				} );
@@ -289,10 +289,10 @@ describe('node-svn-ultimate', function() {
 		});
 		
 		it('lock: remote file in repo', function(done) {
-			svn.commands.lock( testServer + "/test2.txt", function( err, data ) {
+			svn.commands.lock( testServer + '/test2.txt', function( err, data ) {
 				should.not.exist(err);
 				// trying to delete the file should fail
-				svn.commands.del( testServer + "/test2.txt", "del commit", function( err2 ) {
+				svn.commands.del( testServer + '/test2.txt', 'Del test2.txt (locked, fail)', function( err2 ) {
 					should.exist(err2);
 					done();
 				} );
@@ -300,10 +300,10 @@ describe('node-svn-ultimate', function() {
 		});
 		
 		it('unlock: remote file in repo', function(done) {
-			svn.commands.unlock( testServer + "/test2.txt", function( err, data ) {
+			svn.commands.unlock( testServer + '/test2.txt', function( err, data ) {
 				should.not.exist(err);
 				// trying to delete the file should succeed
-				svn.commands.del( testServer + "/test2.txt", "del commit", function( err2 ) {
+				svn.commands.del( testServer + '/test2.txt', 'Del test2.txt (unlocked, success)', function( err2 ) {
 					should.not.exist(err2);
 					done();
 				} );
@@ -311,7 +311,7 @@ describe('node-svn-ultimate', function() {
 		});
 
 		it('mkdir: working copy', function(done) {
-			var p = path.join( checkoutTestDir, "newdir" );
+			var p = path.join( checkoutTestDir, 'newdir' );
 			svn.commands.mkdir( p, function( err ) {
 				should.not.exist(err);
 				should.ok( fs.existsSync( p ) && fs.statSync( p ).isDirectory() );
@@ -319,7 +319,7 @@ describe('node-svn-ultimate', function() {
 			} );
 		});
 		it('mkdir: make array dir', function(done) {
-			var paths = ["trunk", "tags", "branches"].map( function( p ) {
+			var paths = ['trunk', 'tags', 'branches'].map( function( p ) {
 				return path.join( checkoutTestDir, p );
 			});
 			svn.commands.mkdir( paths, function( err ) {
@@ -332,10 +332,10 @@ describe('node-svn-ultimate', function() {
 		});
 		
 		it('mkdir: remote repo', function(done) {
-			svn.commands.mkdir( testServer + "/newdir_remote", 'mkdir commit', function( err ) {
+			svn.commands.mkdir( testServer + '/newdir_remote', 'mkdir remote newdir_remote', function( err ) {
 				should.not.exist(err);
 				// delete dir after
-				svn.commands.del( testServer + "/newdir_remote", "del commit", function( err2 ) {
+				svn.commands.del( testServer + '/newdir_remote', 'Del newdir_remote', function( err2 ) {
 					should.not.exist(err2);
 					done();
 				} );
@@ -351,7 +351,7 @@ describe('node-svn-ultimate', function() {
 		});
 
 		it('propset', function(done) {
-			var p = path.join( checkoutTestDir, "newdir" );
+			var p = path.join( checkoutTestDir, 'newdir' );
 			svn.commands.propset( 'testprop', 'myvalue', p, function( err ) {
 				should.not.exist(err);
 				done();
@@ -359,7 +359,7 @@ describe('node-svn-ultimate', function() {
 		});
 		
 		it('propget', function(done) {
-			var p = path.join( checkoutTestDir, "newdir" );
+			var p = path.join( checkoutTestDir, 'newdir' );
 			svn.commands.propget( 'testprop', p, function( err, data ) {
 				should.not.exist(err);
 				should.exist(data);
@@ -368,7 +368,7 @@ describe('node-svn-ultimate', function() {
 		});
 				
 		it('proplist', function(done) {
-			var p = path.join( checkoutTestDir, "newdir" );
+			var p = path.join( checkoutTestDir, 'newdir' );
 			svn.commands.proplist( p, function( err, data ) {
 				should.not.exist(err);
 				should.exist(data);
@@ -377,7 +377,7 @@ describe('node-svn-ultimate', function() {
 		});
 				
 		it('propdel', function(done) {
-			var p = path.join( checkoutTestDir, "newdir" );
+			var p = path.join( checkoutTestDir, 'newdir' );
 			svn.commands.propdel( 'testprop', p, function( err ) {
 				should.not.exist(err);
 				done();
